f911330b97acde9884290b6c22429b61d19aab55
Your Name <email@example.com>
2025-09-28 22:53:36 +0800

    .

---
+++ Submissions/richardzang/topic1/week2/homework2.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# 1. 读取数据
+charge = pd.read_csv('Submissions/richardzang/topic1/week2/B0005_charge_voltage.csv')
+discharge = pd.read_csv('Submissions/richardzang/topic1/week2/B0005_discharge_voltage.csv')
+
+# 2. 画图
+plt.figure(figsize=(8, 4))
+plt.plot(charge['Time(s)'], charge['Voltage(V)'], label='Charge', color='green')
+plt.plot(discharge['Time(s)'], discharge['Voltage(V)'], label='Discharge', color='red')
+plt.title('B0005 Charge vs Discharge Voltage Curve')
+plt.xlabel('Time (s)')
+plt.ylabel('Voltage (V)')
+plt.legend()
+plt.grid(True)
+plt.tight_layout()
+plt.show()
